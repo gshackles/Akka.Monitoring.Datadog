b8fffee668a99026b7c7c76c303544630c714964
Greg Shackles <gshackles@gmail.com>
2020-07-01 07:28:50 -0700

    convert all projects to SDK style

--- src/Akka.Monitoring.Datadog/Properties/AssemblyInfo.cs
+++ src/Akka.Monitoring.Datadog/Properties/AssemblyInfo.cs
@@ -1,9 +1,2 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Akka.Monitoring.Datadog")]
-[assembly: AssemblyDescription("Akka.Monitoring monitor for Datadog")]
-[assembly: AssemblyCopyright("2017 Greg Shackles")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+﻿using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Akka.Monitoring.Datadog.Tests")]
